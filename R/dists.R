36b17a795facca94660abf0a1056c87e5e94c1b4
mpadge <mark.padgham@email.com>
2019-01-09 17:35:42 +0100

    message from extendedTri only if \!quiet

--- R/dists.R
+++ R/dists.R
@@ -142,8 +142,9 @@ dodgr_dists <- function (graph, from, to, wt_profile = "bicycle", expand = 0,
 
     if (parallel & heap == "TriHeapExt")
     {
-        message ("Extended TriHeaps can not be calculated in parallel; ",
-                 "reverting to serial calculation")
+        if (!quiet)
+            message ("Extended TriHeaps can not be calculated in parallel; ",
+                     "reverting to serial calculation")
         parallel <- FALSE
     }
 
